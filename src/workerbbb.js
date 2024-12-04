d35da62eb07f51c22bfbdb386c25ceaa86ce1c9b
Hellog <57588023+luxiangchuan@users.noreply.github.com>
2024-12-04 16:35:42 +0800

    Update workerbbb.js

--- src/workerbbb.js
+++ src/workerbbb.js
@@ -1,12 +1,12 @@
 import { initializeParams } from './helpers/init';
-import { vlessOverWSHandler } from './protocols/vless';
-import { trojanOverWSHandler } from './protocols/trojan';
-import { updateWarpConfigs } from './kv/handlers';
+//import { vlessOverWSHandler } from './protocols/vless';
+//import { trojanOverWSHandler } from './protocols/trojan';
+//import { updateWarpConfigs } from './kv/handlers';
 import { logout, resetPassword, login } from './authentication/auth';
 import { renderErrorPage } from './pages/error';
-import { getXrayCustomConfigs, getXrayWarpConfigs } from './cores-configs/xray';
-import { getSingBoxCustomConfig, getSingBoxWarpConfig } from './cores-configs/sing-box';
-import { getClashNormalConfig, getClashWarpConfig } from './cores-configs/clash';
+//import { getXrayCustomConfigs, getXrayWarpConfigs } from './cores-configs/xray';
+//import { getSingBoxCustomConfig, getSingBoxWarpConfig } from './cores-configs/sing-box';
+//import { getClashNormalConfig, getClashWarpConfig } from './cores-configs/clash';
 import { getNormalConfigs } from './cores-configs/normalConfigs';
 import { fallback, getMyIP, handlePanel } from './helpers/helpers';
 import { renderSecretsPage } from './pages/secrets';
@@ -18,15 +18,15 @@ export default {
             const upgradeHeader = request.headers.get('Upgrade');
             if (!upgradeHeader || upgradeHeader !== 'websocket') {            
                 switch (globalThis.pathName) {                    
-                    case '/update-warp':
+/*                    case '/update-warp':
                         return await updateWarpConfigs(request, env);
-
+*/
                     case `/sub/${globalThis.userID}`:
-                        if (globalThis.client === 'sfa') return await getSingBoxCustomConfig(request, env, false);
-                        if (globalThis.client === 'clash') return await getClashNormalConfig(request, env);
-                        if (globalThis.client === 'xray') return await getXrayCustomConfigs(request, env, false);
+                        //if (globalThis.client === 'sfa') return await getSingBoxCustomConfig(request, env, false);
+                        //if (globalThis.client === 'clash') return await getClashNormalConfig(request, env);
+                        //if (globalThis.client === 'xray') return await getXrayCustomConfigs(request, env, false);
                         return await getNormalConfigs(request, env);                        
-
+/*
                     case `/fragsub/${globalThis.userID}`:
                         return globalThis.client === 'hiddify'
                             ? await getSingBoxCustomConfig(request, env, true)
@@ -36,7 +36,7 @@ export default {
                         if (globalThis.client === 'clash') return await getClashWarpConfig(request, env);   
                         if (globalThis.client === 'singbox' || globalThis.client === 'hiddify') return await getSingBoxWarpConfig(request, env, globalThis.client);
                         return await getXrayWarpConfigs(request, env, globalThis.client);
-
+*/
                     case '/panel':
                         return await handlePanel(request, env);
                                                       
@@ -48,10 +48,10 @@ export default {
 
                     case '/panel/password':
                         return await resetPassword(request, env);
-                    
+ /*                   
                     case '/my-ip':
                         return await getMyIP(request);
-
+*/
                     case '/secrets':
                         return await renderSecretsPage();
 
@@ -59,10 +59,10 @@ export default {
                         return await fallback(request);
                 }
             } else {
-                return globalThis.pathName.startsWith('/tr') 
+/*                return globalThis.pathName.startsWith('/tr') 
                     ? await trojanOverWSHandler(request) 
                     : await vlessOverWSHandler(request);
-            }
+*/            }
         } catch (err) {
             return await renderErrorPage(err);
         }
